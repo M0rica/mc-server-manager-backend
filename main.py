e9c5dc4eb0f2826ac62c39fef272c0295cb3d6d5
M0rica <afer2003.mueller@gmail.com>
2022-01-28 19:22:11 +0100

    open website on startup

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
+import webbrowser
+
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
 
 import uvicorn
 from starlette.staticfiles import StaticFiles
 
-from api import file_loader
+from api import server_routing
 
 from api import api
 
 app = FastAPI()
 
 app.include_router(api.router)
-app.include_router(file_loader.router)
+app.include_router(server_routing.router)
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
@@ -25,5 +27,6 @@ async def root():
 
 
 if __name__ == '__main__':
-    uvicorn.run("main:app", host="0.0.0.0", workers=2, port=5000)
+    webbrowser.open('localhost:5000')
+    uvicorn.run("main:app", host="0.0.0.0", workers=2, port=5000, reload=True)
 
