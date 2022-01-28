0e22bb6c55feaf8d378628081d21425a8b820273
M0rica <afer2003.mueller@gmail.com>
2022-01-28 19:21:04 +0100

    delete old structure

--- paths.py
+++
@@ -1,9 +0,0 @@
-import os
-
-data_path = os.path.expanduser("~/.local/share/mc-manager")
-
-if not os.path.exists(data_path):
-    os.makedirs(data_path)
-
-if not os.path.exists(p := os.path.join(data_path, "jars")):
-    os.mkdir(p)
